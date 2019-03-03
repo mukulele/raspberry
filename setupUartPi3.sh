1cd17ac655b56ad6257cf72abe2783353224310a
heinz-otto <heinz-otto.klas@gmx.de>
2019-03-03 18:03:22 +0100

    Create setupUartPi3.sh
    Prepare UART Interface an Raspberry Pi3 for use with GPIO Modules

---
+++ setupUartPi3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# deactivate serial-getty@ttyAMA0.service 
+systemctl stop serial-getty@ttyAMA0.service
+systemctl disable serial-getty@ttyAMA0.service
+systemctl mask serial-getty@ttyAMA0.service
+# aktivate UART and switch with BT Interface
+cat <<EOF >> config.txt
+enable_uart=1
+dtoverlay=pi3-miniuart-bt
+core_freq=250
+EOF
