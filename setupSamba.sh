a585c735c8619efea98e9a7c3a37f12f0c963f43
heinz-otto <heinz-otto.klas@gmx.de>
2019-03-03 21:27:56 +0100

    Create setupSamba.sh

---
+++ setupSamba.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+spath="/var/SonosSpeak"
+apt-get install samba
+
+# Pfad erstellen
+mkdir $spath
+chmod 777 $spath
+
+# Share Definition in /etc/samba/smb.conf
+cat <<EOF >> /etc/samba/smb.conf
+[SonosSpeak]
+  comment = Audio-Files for SonosPlayer to Speak
+  path = $spath
+  browsable =yes
+  guest ok yes
+  read only = no
+EOF
+
+# restart Service
+systemctl restart smbd
