3daf440aa1b48492381caae02b526dc3f8a6e01f
heinz-otto <heinz-otto.klas@gmx.de>
2019-03-03 18:16:24 +0100

    Update setupUartPi3.sh

--- setupUartPi3.sh
+++ setupUartPi3.sh
@@ -4,7 +4,7 @@ systemctl stop serial-getty@ttyAMA0.service
 systemctl disable serial-getty@ttyAMA0.service
 systemctl mask serial-getty@ttyAMA0.service
 # aktivate UART and switch with BT Interface
-cat <<EOF >> config.txt
+cat <<EOF >> /boot/config.txt
 enable_uart=1
 dtoverlay=pi3-miniuart-bt
 core_freq=250
