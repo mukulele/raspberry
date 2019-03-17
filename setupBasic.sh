4f13bfd8516e5966e9575ccae3c87cc68ea52cfc
Otto <heinz-otto.klas@gmx.de>
2019-03-17 18:27:30 +0100

    Update setupBasic.sh
    Add asking for Hostname

--- setupBasic.sh
+++ setupBasic.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# replace the name "mymachine" lines below
+# Set the new hostname below for quiet setup or leave it blank to be asked
+hname=
+
 # Timezone
 timedatectl set-timezone Europe/Berlin
 
@@ -11,7 +13,16 @@ localectl set-locale LANG=de_DE.UTF-8 LANGUAGE=de_DE
 sed -i '1 i\country=DE' /etc/wpa_supplicant/wpa_supplicant.conf
 
 # Hostname 
-hostnamectl set-hostname mymachine
+if [ -z $hname ]
+	then
+		hname=$(hostname)
+		read -p "Hostname "$hname" ändern? Bitte den neuen Hostnamen eingeben:" hnameneu
+		if [ -n $hnameneu ]
+			then
+				hname=$hnameneu
+		fi
+fi
+hostnamectl set-hostname $hname
 
 # Reboot
 reboot
