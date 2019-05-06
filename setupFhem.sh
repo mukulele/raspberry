ea750f28311553ff36869cf8054b332826b2825f
Otto <heinz-otto.klas@gmx.de>
2019-05-06 12:34:18 +0200

    Update setupFhem.sh

--- setupFhem.sh
+++ setupFhem.sh
@@ -1,18 +1,35 @@
+#!/bin/bash
+#functions
+# getFile Filename RepositoryName
+function getFile {
+  if [ ! -e \$1 ]
+  then
+    echo "\$1 is missing"
+    wget https://raw.githubusercontent.com/heinz-otto/\$2/master/\$1
+    chmod +x \$1
+  fi
+}  
 # von debian.fhem.de installieren - siehe aktuelle Anleitung dort https://debian.fhem.de/
 wget -qO - http://debian.fhem.de/archive.key | apt-key add -
 echo "deb http://debian.fhem.de/nightly/ /" >> /etc/apt/sources.list
 apt-get update
 apt-get upgrade
+
 # get additional Package Files
-wget -O fhemDeb.txt https://raw.githubusercontent.com/heinz-otto/raspberry/master/fhemDeb.txt
-wget -O fhemCpan.txt https://raw.githubusercontent.com/heinz-otto/raspberry/master/fhemCpan.txt
+# check if local Package Files exist
+getFile fhemDeb.txt raspberry
+getFile fhemCpan.txt raspberry
+
 # install debian packages
 apt-get -y install $(cat fhemDeb.txt |tr -d "\r"|tr "\n" " ")
 # install Cpan packages
 cpan install $(cat fhemCpan.txt |tr -d "\r"|tr "\n" " ")
+
 # Setup FHEM
 apt-get -y install fhem
-wget -O fhemcl.sh https://raw.githubusercontent.com/heinz-otto/fhemcl/master/fhemcl.sh
+# get the HTTP Client
+getFile fhemcl.sh fhemcl
+# setup a Basic Configuration
 cat <<EOF | bash fhemcl.sh 8083
 attr initialUsbCheck disable 1
 attr WEB JavaScripts codemirror/fhem_codemirror.js
