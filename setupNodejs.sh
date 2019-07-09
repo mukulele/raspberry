364a348dd6f099d2d47c330e21e48178a85ce5ed
Otto <heinz-otto.klas@gmx.de>
2019-07-09 17:51:02 +0200

    Create setupNodejs.sh

---
+++ setupNodejs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# look for Releases https://nodejs.org/de/about/releases/
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt-get install -y nodejs
+apt-get install -y libjson-pp-perl
+npm install pm2 -g
