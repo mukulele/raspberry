f9658b89d7d61b88097418f0e13b493afe982f7e
Otto <heinz-otto.klas@gmx.de>
2019-12-28 23:35:14 +0100

    Update setupUartPi3.sh

--- setupUartPi3.sh
+++ setupUartPi3.sh
@@ -3,11 +3,12 @@
 systemctl stop serial-getty@ttyAMA0.service
 systemctl disable serial-getty@ttyAMA0.service
 systemctl mask serial-getty@ttyAMA0.service
-# aktivate UART and switch with BT Interface
+# aktivate UART and switch miniUart to BT Interface
 # Since buster the overlay ist renamed but the old name is still linked
 # I removed enable_uart=1 because this will be done by the overlay
-# I removed the line core_freq=250 see documentation for details. I am not shure at this moment
+# The line core_freq=250 is needed otherwise BT is not working - see documentation for details.
 # https://www.raspberrypi.org/documentation/configuration/config-txt/overclocking.md
 cat <<EOF >> /boot/config.txt
 dtoverlay=miniuart-bt
+core_freq=250
 EOF
