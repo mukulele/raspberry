5526b591184a3a883fb7bd8bbf2e8e0046dfa458
Otto <heinz-otto.klas@gmx.de>
2020-07-04 21:12:41 +0200

    Update setupFhem.sh

--- setupFhem.sh
+++ setupFhem.sh
@@ -45,7 +45,7 @@ dat=($(cat /etc/resolv.conf|grep nameserver))
 cat <<EOF | bash fhemcl.sh 8083
 attr initialUsbCheck disable 1
 attr WEB JavaScripts codemirror/fhem_codemirror.js
-attr WEB codemirrorParam { "theme":"blackboard", "lineNumbers":true }
+attr WEB codemirrorParam { "lineNumbers":true }
 attr WEB plotfork 1
 attr WEB longpoll websocket
 attr global backup_before_update 1
