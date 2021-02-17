171affe60b23b76eadf7ce5ac3e01bdc17584aa9
Otto <heinz-otto.klas@gmx.de>
2021-02-17 19:11:14 +0100

    Update setupDocker.sh

--- setupDocker.sh
+++ setupDocker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # docker itselfs
 curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker pi
+sudo usermod -aG docker $(whoami)
 # docker-compose the actual version
 sudo apt-get install -y libffi-dev libssl-dev
 sudo apt-get install -y python3 python3-pip
-sudo apt-get remove python-configparser
 sudo pip3 install docker-compose
 # portainer as solo container - must be sudo because group docker not working without logout/login
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 # make a working directory 
 mkdir docker
-echo "your Docker Setup is ready \n Logout and Login for Groupmembership\n after reconnect could \n  cd docker"
+echo -e "Hello $(whoami) - your Docker Setup is ready \n Logout and Login for docker Groupmembership\n after reconnect you could \n  cd docker"
+echo -e "Portainer is running and reachable under\n   http://$(hostname):9000"
