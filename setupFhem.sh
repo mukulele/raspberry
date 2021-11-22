6ecfe667e93f14d20592e9eaa349dc5b3391e28a
Otto <heinz-otto.klas@gmx.de>
2021-11-22 16:28:03 +0100

    Update setupFhem.sh

--- setupFhem.sh
+++ setupFhem.sh
@@ -30,13 +30,23 @@ cpan install $(cat fhemCpan.txt |grep -v '#'|tr -d "\r"|tr "\n" " ")
 # von debian.fhem.de installieren - siehe aktuelle Anleitung dort https://debian.fhem.de/
 # temporärer Workaround wenn mal das Paket nicht signiert ausgeliefert wird   
 # echo "deb [trusted=yes] http://debian.fhem.de/nightly/ /" >> /etc/apt/sources.list
-if [[ "$(apt list fhem)" =~ "installed" ]];then
+if [[ "$(apt list fhem)" =~ "installed" ]] ;then
     echo fhem ist bereits installiert
 else
-# debian Version abfragen
-# if [ "$(lsb_release -s -r | cut -d '.' -f 1)" -lt "10" ] ;then echo "Release kleiner 10";else echo "release passt"; fi
-  if [ "$(wget -qO - http://debian.fhem.de/archive.key | apt-key add -)" = "OK" ];then
-    echo "deb http://debian.fhem.de/nightly/ /" >> /etc/apt/sources.list
+  # get debian version strings with dot sourcing
+  . /etc/os-release
+  if [ $VERSION_ID -ge 10 ] ;then 
+    if wget -qO - https://debian.fhem.de/archive.key | gpg --dearmor > /usr/share/keyrings/debianfhemde-archive-keyring.gpg ;then
+      echo "deb [signed-by=/usr/share/keyrings/debianfhemde-archive-keyring.gpg] https://debian.fhem.de/nightly/ /" >> /etc/apt/sources.list
+      key='ok'
+    fi
+  else 
+    if [ "$(wget -qO - http://debian.fhem.de/archive.key | apt-key add -)" = "OK" ] ;then
+      echo "deb http://debian.fhem.de/nightly/ /" >> /etc/apt/sources.list
+      key='ok'
+    fi
+  fi
+  if [ $key = 'ok' ] ;then
     apt-get update
     apt-get install fhem
   else
