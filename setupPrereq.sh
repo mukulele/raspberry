b2e4c7b3a5a91b524032924876f13bffa20414f9
Otto <heinz-otto.klas@gmx.de>
2021-12-09 19:12:29 +0100

    Update setupPrereq.sh

--- setupPrereq.sh
+++ setupPrereq.sh
@@ -12,55 +12,78 @@ function getFile {
     chmod +x $1
   fi
 }
-# System aufrüsten
-apt update
-apt install apt-file libperl-prereqscanner-notquitelite-perl
-apt-file update
-
-if [[ "$(apt list fhem)" =~ "installed" ]] ;then
-    echo fhem ist bereits installiert
-else
-  # get debian version strings with dot sourcing
-  . /etc/os-release
+function setup-Fhem {
+# get debian version strings with dot sourcing
+. /etc/os-release
   if [ $VERSION_ID -ge 10 ] ;then
     apt install gpg
     if wget -qO - https://debian.fhem.de/archive.key | gpg --dearmor > /usr/share/keyrings/debianfhemde-archive-keyring.gpg ;then
       echo "deb [signed-by=/usr/share/keyrings/debianfhemde-archive-keyring.gpg] https://debian.fhem.de/nightly/ /" >> /etc/apt/sources.list
       key='ok'
     fi
-  else 
+  else
     if [ "$(wget -qO - http://debian.fhem.de/archive.key | apt-key add -)" = "OK" ] ;then
       echo "deb http://debian.fhem.de/nightly/ /" >> /etc/apt/sources.list
       key='ok'
     fi
   fi
   if [ $key = 'ok' ] ;then
-    apt-get update
-    apt-get install fhem
+    apt update
+    apt install fhem
   else
     echo Es gab ein Problem mit dem debian.fhem.de/archive.key
     exit 1
   fi
+
+}
+
+function analyze-config {
+# Abfrage starten
+s=$(./fhemcl.sh 8083 "get installer checkPrereqs $1"|grep -oE 'installPerl.*&fwcsrf'|grep -oE '\s[a-z,A-Z,:]+\s')
+packages=$(echo $s|tr " " "\n"|sed 's/$/./;s/^/\//'|apt-file search -l -f -)
+
+# fehlende Pakete installieren
+echo "es fehlen Pakete"
+echo $packages
+echo 'apt install $packages'
+}
+# System aufrüsten
+PKG="libperl-prereqscanner-notquitelite-perl"
+if dpkg-query -l $PKG > /dev/null
+ then
+      echo "System schon aufgerüstet"
+  else
+      apt update
+      apt install apt-file libperl-prereqscanner-notquitelite-perl
+      apt-file update
 fi
+# fhem installieren
+PKG="fhem"
+dpkg-query -l $PKG > /dev/null || setup-Fhem
 
 # get the HTTP Client
 getFile fhemcl.sh fhemcl
 
 # Definition zum Testen erstellen
+if [[ "$(./fhemcl.sh 8083 "list installer installerMode")" =~ "developer" ]] ;then
+    echo "Installermodul bereits eingerichtet"
+else
 cat <<EOF | ./fhemcl.sh 8083
 attr initialUsbCheck disable 1
-define installer installer
+defmod installer Installer
 attr installer installerMode developer
 save
 EOF
 
-exit
+fi
 
-# Abfrage starten
-s=$(./fhemcl.sh 8083 "get installer checkPrereqs $ref"|grep -oE 'installPerl.*&fwcsrf'|grep -oE '\s[a-z,A-Z,:]+\s')
-packages=$(echo $s|tr " " "\n"|sed 's/$/./;s/^/\//'|apt-file search -l -f -)
+if [[ $ref = "" ]]
+  then
+  read -p "Dateiname eingeben:"ref
+fi
 
-# fehlende Pakete installieren
-echo "es fehlen Packete"
-echo $packages
-echo 'apt install $packages'
+if [ -s $ref ]
+then
+  echo "Analyse mit Datei $ref wird gestartet"
+  analyze-config $ref
+fi
