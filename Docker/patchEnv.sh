19c315d52f5e89c946abfcb9cb12ba8c187cee57
Otto <heinz-otto.klas@gmx.de>
2021-12-16 11:35:11 +0100

    Update patchEnv.sh

--- Docker/patchEnv.sh
+++ Docker/patchEnv.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # patch .env File with real data
-# for test remove -i option
-sed -i s/SONOS2MQTT_DEVICE=.*$/SONOS2MQTT_DEVICE=$(host -4 -W1 -t A  SonosZP | awk '/has address/ { print $NF; exit }')/ .env
-sed -i "s/SONOS2MQTT_MQTT=.*$/SONOS2MQTT_MQTT=mqtt:\/\/$(hostname):1883/" .env
-sed -i "s/SONOS_LISTENER_HOST=.*$/SONOS_LISTENER_HOST=$(hostname)/" .env
-sed -i "s/CONBEE=.*$/CONBEE=$(ls /dev/serial/by-id/|grep ConBee)/" .env
+
+option='-i' # for replacements inline
+option=''   # for test only sed result
+sed $option s/SONOS2MQTT_DEVICE=.*$/SONOS2MQTT_DEVICE=$(host -4 -W1 -t A  SonosZP | awk '/has address/ { print $NF; exit }')/ .env
+sed $option "s/SONOS2MQTT_MQTT=.*$/SONOS2MQTT_MQTT=mqtt:\/\/$(hostname):1883/" .env
+sed $option "s/SONOS_LISTENER_HOST=.*$/SONOS_LISTENER_HOST=$(hostname)/" .env
+sed $option "s/CONBEE=.*$/CONBEE=$(ls /dev/serial/by-id/|grep ConBee)/" .env
