c9ebc460ed43eb4ffee34fe93689dbcbd3f4b232
Otto <heinz-otto.klas@gmx.de>
2021-12-16 12:06:30 +0100

    Update setupDocker.sh

--- setupDocker.sh
+++ setupDocker.sh
@@ -8,9 +8,9 @@ sudo usermod -aG docker $(whoami)
 sudo apt-get install -y libffi-dev libssl-dev
 sudo apt-get install -y python3 python3-pip
 sudo pip3 install docker-compose
-# portainer as solo container - must be sudo because group docker not working without logout/login
-sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+# portainer as solo container - sudo -su $(id -un) give the trick to get new groupmembership without extra logout/login
+sudo -su $(id -un) docker volume create portainer_data
+sudo -su $(id -un) docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 # make a working directory 
 mkdir docker
 echo -e "Hello $(whoami) - your Docker Setup is ready \n Logout and Login for docker Groupmembership\n after reconnect you could \n  cd docker"
