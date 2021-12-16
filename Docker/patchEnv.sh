91e95e6b4a85f1472e2a9abb965e5a4bb51f69a7
Otto <heinz-otto.klas@gmx.de>
2021-12-16 11:23:27 +0100

    Update patchEnv.sh

--- Docker/patchEnv.sh
+++ Docker/patchEnv.sh
@@ -4,3 +4,4 @@
 sed -i s/SONOS2MQTT_DEVICE=.*$/SONOS2MQTT_DEVICE=$(host -4 -W1 -t A  SonosZP | awk '/has address/ { print $NF; exit }')/ .env
 sed -i "s/SONOS2MQTT_MQTT=.*$/SONOS2MQTT_MQTT=mqtt:\/\/$(hostname):1883/" .env
 sed -i "s/SONOS_LISTENER_HOST=.*$/SONOS_LISTENER_HOST=$(hostname)/" .env
+sed -i "s/CONBEE=.*$/CONBEE=$(ls /dev/serial/by-id/|grep ConBee)/" .env
