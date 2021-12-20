af640c7451cfbc5b5e4eb5d55835b190c94e7de6
Otto <heinz-otto.klas@gmx.de>
2021-12-20 21:52:28 +0100

    Update setupDockerComposeV2.sh

--- Docker/setupDockerComposeV2.sh
+++ Docker/setupDockerComposeV2.sh
@@ -1,9 +1,14 @@
-docker-compose version||echo "docker-compose nicht gefunden"
-# first try to remove docker-compose
-sudo rm /usr/local/bin/docker-compose > /dev/null 2>&1 && echo "/usr/local/bin/docker-compose entfernt"
-sudo pip3 uninstall docker-compose > /dev/null 2>&1 && echo "docker-compose mit pip3 deinstalliert"
-sudo apt purge docker-compose && sudo apt autoremove ; echo "docker-compose mit apt deinstalliert"
-
+#!/bin/sh
+# install docker compose v2 plugin
+# first test and try to remove docker-compose v1
+if docker-compose version 
+the
+  sudo rm /usr/local/bin/docker-compose > /dev/null 2>&1 && echo "/usr/local/bin/docker-compose entfernt"
+  sudo pip3 uninstall docker-compose > /dev/null 2>&1 && echo "docker-compose mit pip3 deinstalliert"
+  sudo apt purge docker-compose && sudo apt autoremove ; echo "docker-compose mit apt deinstalliert"
+else
+  echo "docker-compose nicht gefunden"
+fi
 # docker compose v2 plugin setup https://docs.docker.com/compose/cli-command/#install-on-linux
 # Replace with the latest version from https://github.com/docker/compose/releases/latest
 COMPOSE_VER="2.2.2"
@@ -18,3 +23,5 @@ DOWNLOAD_PATH="https://github.com/docker/compose/releases/download/v${COMPOSE_VE
 mkdir -p ${PLUGIN_PATH}
 curl -SL ${DOWNLOAD_PATH} -o ${PLUGIN_PATH}docker-compose
 chmod +x ${PLUGIN_PATH}docker-compose
+# test success
+docker compose version
