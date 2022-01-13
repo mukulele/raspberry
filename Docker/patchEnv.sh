934a36e2d030f924cd8163b8bb0413ad53fb394d
Otto <heinz-otto.klas@gmx.de>
2022-01-13 13:32:45 +0100

    Update patchEnv.sh

--- Docker/patchEnv.sh
+++ Docker/patchEnv.sh
@@ -7,3 +7,4 @@ sed $option s/SONOS2MQTT_DEVICE=.*$/SONOS2MQTT_DEVICE=$(host -4 -W1 -t A  SonosZ
 sed $option "s/SONOS2MQTT_MQTT=.*$/SONOS2MQTT_MQTT=mqtt:\/\/$(hostname):1883/" .env
 sed $option "s/SONOS_LISTENER_HOST=.*$/SONOS_LISTENER_HOST=$(hostname)/" .env
 sed $option "s/CONBEE=.*$/CONBEE=$(ls /dev/serial/by-id/|grep ConBee)/" .env
+#echo "CONBEE=$(ls /dev/serial/by-id/|grep ConBee)/" >>.env
