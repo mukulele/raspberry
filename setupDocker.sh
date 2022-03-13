27012fcd3f276a30e8833a153d17d55b183e56ed
Otto <heinz-otto.klas@gmx.de>
2022-03-13 09:10:26 +0100

    Update setupDocker.sh

--- setupDocker.sh
+++ setupDocker.sh
@@ -31,10 +31,13 @@ chmod +x ${PLUGIN_PATH}docker-compose
 #sudo apt-get install -y libffi-dev libssl-dev
 #sudo apt-get install -y python3 python3-pip
 #sudo pip3 install docker-compose
-# portainer as solo container - sudo -su $(id -un) give the trick to get new groupmembership without extra logout/login
-sudo -su $(id -un) docker volume create portainer_data
-sudo -su $(id -un) docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+read -p "portainer installieren?, Yy oder einfach enter:" input
+	if [[ $input != "" ]] ;	then
+      # portainer as solo container - sudo -su $(id -un) give the trick to get new groupmembership without extra logout/login
+      sudo -su $(id -un) docker volume create portainer_data
+      sudo -su $(id -un) docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+	fi
 # make a working directory 
-mkdir docker
+mkdir -p "$HOME/docker"
 echo -e "Hello $(whoami) - your Docker Setup is ready \n Logout and Login for docker Groupmembership\n after reconnect you could \n  cd docker"
 echo -e "Portainer is running and reachable under\n   http://$(hostname):9000"
