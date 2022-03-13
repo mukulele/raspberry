10ff4a9e69240d7e4659b6720ec15d3e144b800e
Otto <heinz-otto.klas@gmx.de>
2022-03-13 20:38:20 +0100

    Update setupDocker.sh

--- Docker/setupDocker.sh
+++ Docker/setupDocker.sh
@@ -7,3 +7,7 @@ sh get-docker.sh
 # add user to group
 usermod -aG docker $(whoami)
 HERE
+# for group membership new login
+echo "open now a new shell for docker groupmembership"
+echo "after exit you need to re login"
+sudo -su $(id -un)
