8d6a2c278c2a623d2cb25601635094865e9cfd92
Otto <heinz-otto.klas@gmx.de>
2022-03-14 13:58:49 +0100

    Update buildFHEM.sh

--- Docker/buildFHEM.sh
+++ Docker/buildFHEM.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # build docker and fhem image from scratch
-if ! docker -v &>/dev/null ; then
+IMAGE_NAME='debianfhem'
+if ! docker -v 2>/dev/null ; then
    wget -qO setupDocker.sh https://raw.githubusercontent.com/heinz-otto/raspberry/master/Docker/setupDocker.sh
    bash setupDocker.sh
 fi
 sudo -su $(id -un) <<HERE
-if ! docker compose version &>/dev/null ; then
+if ! docker compose version 2>/dev/null ; then
    wget -qO setupDockerComposeV2.sh https://raw.githubusercontent.com/heinz-otto/raspberry/master/Docker/setupDockerComposeV2.sh
    bash setupDockerComposeV2.sh
 fi
 mkdir -p "$HOME/docker"
-docker build -t debianfhem https://raw.githubusercontent.com/heinz-otto/fhem-docker/main/DockerfileDeb
-docker run -v "$HOME/docker/fhem:/opt/fhem"  --name testfhem debianfhem init svn clean
+docker build -t ${IMAGE_NAME} https://raw.githubusercontent.com/heinz-otto/fhem-docker/main/DockerfileDeb
+docker run -v "$HOME/docker/fhem:/opt/fhem"  --name testfhem ${IMAGE_NAME} init svn clean
 docker container rm testfhem
 HERE
-echo 'sudo -su $(id -un) docker run -v "$HOME/docker/fhem:/opt/fhem" -p "8083:8083" --name testfhem debianfhem'
+echo 'sudo -su $(id -un) docker run -v "$HOME/docker/fhem:/opt/fhem" -p "8083:8083" --name testfhem ${IMAGE_NAME}'
 echo 'or logoff and logon again for further work with docker without sudo -su $(id -un)'
