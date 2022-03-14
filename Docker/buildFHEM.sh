c86c4a8ed23dc6cddba95d8cc3077c55e7d7094f
Otto <heinz-otto.klas@gmx.de>
2022-03-14 13:43:07 +0100

    Update buildFHEM.sh

--- Docker/buildFHEM.sh
+++ Docker/buildFHEM.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # build docker and fhem image from scratch
 if ! docker -v &>/dev/null ; then
-   wget https://raw.githubusercontent.com/heinz-otto/raspberry/master/Docker/setupDocker.sh
+   wget -qO setupDocker.sh https://raw.githubusercontent.com/heinz-otto/raspberry/master/Docker/setupDocker.sh
    bash setupDocker.sh
 fi
 sudo -su $(id -un) <<HERE
 if ! docker compose version &>/dev/null ; then
-   wget https://raw.githubusercontent.com/heinz-otto/raspberry/master/Docker/setupDockerComposeV2.sh
+   wget -qO setupDockerComposeV2.sh https://raw.githubusercontent.com/heinz-otto/raspberry/master/Docker/setupDockerComposeV2.sh
    bash setupDockerComposeV2.sh
 fi
 mkdir -p "$HOME/docker"
