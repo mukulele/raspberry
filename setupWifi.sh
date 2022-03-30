2cf9cd5b20721cf62c04df7e3b4138105415935b
Otto <heinz-otto.klas@gmx.de>
2022-03-30 13:40:31 +0200

    Create setupWifi.sh

---
+++ setupWifi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# setup basic Wifi dor country DE
+# Set Country for WiFi
+if [ -n "$(iw dev)" ] 
+    then 
+	iw reg set DE
+	# add first line for WiFi if not already there
+	file="/etc/wpa_supplicant/wpa_supplicant.conf"
+	zn=$(sed -n '/country/=' $file) # ermittelt Zeilenummer mit country
+	if [ -z $zn ]
+		then
+		      sed -i '1 i\country=DE' $file
+	fi
+fi
