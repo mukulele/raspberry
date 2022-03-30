8fbb4522f2bf573bf97707a83c909690272a7951
Otto <heinz-otto.klas@gmx.de>
2022-03-30 16:13:05 +0200

    Update and rename setupUartPi3.sh to setupUart.sh

---
+++ setupUart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Since buster the overlay ist renamed but the old name is still linked
+# I kept enable_uart=1, altough it's described as done inside the overlay.
+# The line core_freq=250 is needed otherwise BT is not working - see documentation for details.
+# https://www.raspberrypi.org/documentation/configuration/uart.md
+# https://www.raspberrypi.org/documentation/configuration/config-txt/overclocking.md
+
+# deactivate serial-getty@ttyAMA0.service
+for cmd in stop disable mask ; do 
+    systemctl $cmd serial-getty@ttyAMA0.service
+done
+# aktivate UART 
+echo 'enable_uart=1' >> /boot/config.txt
+
+# switch miniUart to BT Interface for Pi Model 3, 4, Zero W and Zero WH
+model=($(tr -d '\0' < /sys/firmware/devicetree/base/model))
+if (( ${model[2]} >= 3 )) || ( [[ ${model[2]} == "Zero" ]] && [[ ${model[3]:0:1} == "W" ]] ); then
+   cat <<EOF >> /boot/config.txt
+dtoverlay=miniuart-bt
+core_freq=250
+EOF
+fi
