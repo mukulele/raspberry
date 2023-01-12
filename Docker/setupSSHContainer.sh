dd125a6e00d0b4f2f5c7438f4da022798042f6f0
Otto <heinz-otto.klas@gmx.de>
2023-01-12 19:42:36 +0100

    Update setupSSHContainer.sh

--- Docker/setupSSHContainer.sh
+++ Docker/setupSSHContainer.sh
@@ -1,2 +1,10 @@
-echo "Zielhost ${ziel_host}"
-ls -lha ~/.ssh/
+for ziel in ${ziel_host} $(ping -4c 1 ${ziel_host} | awk 'NR==1{gsub(/\(|\)/,"",$3);print $3}') ; do
+  yes '' | ssh-keygen -R \${ziel}
+  ssh-keyscan -t ed25519 \${ziel} 2>/dev/null >> ~/.ssh/known_hosts
+done
+if ! ls ~/.ssh/id_rsa.pub >/dev/null; then
+    ssh-keygen -f ~/.ssh/id_rsa -P "" -t rsa
+  else
+    echo "rsa key already in place"
+fi
+sort -u ~/.ssh/known_hosts -o ~/.ssh/known_hosts
