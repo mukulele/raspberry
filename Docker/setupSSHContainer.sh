f037d7e50c6caacf0ada90d8514fab45a80a08a0
Otto <heinz-otto.klas@gmx.de>
2023-01-12 23:36:34 +0100

    Update setupSSHContainer.sh

--- Docker/setupSSHContainer.sh
+++ Docker/setupSSHContainer.sh
@@ -1,6 +1,6 @@
 for ziel in ${ziel_host} $(ping -4c 1 ${ziel_host} | awk 'NR==1{gsub(/\(|\)/,"",$3);print $3}') ; do
-  yes '' | ssh-keygen -R \${ziel}
-  ssh-keyscan -t ed25519 \${ziel} 2>/dev/null >> ~/.ssh/known_hosts
+  yes '' | ssh-keygen -R ${ziel}
+  ssh-keyscan -t ed25519 ${ziel} 2>/dev/null >> ~/.ssh/known_hosts
 done
 if ! ls ~/.ssh/id_rsa.pub >/dev/null; then
     ssh-keygen -f ~/.ssh/id_rsa -P "" -t rsa
