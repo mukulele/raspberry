e678cdaf61ee6554ae7f40b312af499f97cf88ef
Otto <heinz-otto.klas@gmx.de>
2023-01-17 16:26:01 +0100

    Update setupSSHContainer.sh

--- Docker/setupSSHContainer.sh
+++ Docker/setupSSHContainer.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # remove and set the public key from hostnames as arguments
 # create a new public for the current user if not exists
-# remove doubles in known_hosts - ist das nicht unnötig?
-key_version=ed25519 #rsa, ed25519, dsa (unsafe)
+# print out the public key 
+# set version rsa, ed25519, dsa (unsafe)
+key_version=ed25519 
 for arg in $* ; do
   yes '' | ssh-keygen -R ${arg}
-  ssh-keyscan -t ed25519 ${arg} 2>/dev/null >> ~/.ssh/known_hosts
+  ssh-keyscan -t ${key_version} ${arg} 2>/dev/null >> ~/.ssh/known_hosts
 done
 if ! ls ~/.ssh/id_${key_version}.pub >/dev/null; then
     ssh-keygen -f ~/.ssh/id_${key_version} -P "" -t ${key_version}
   else
     echo "public key of type ${key_version} already in place"
 fi
-#sort -u ~/.ssh/known_hosts -o ~/.ssh/known_hosts
 cat ~/.ssh/id_${key_version}.pub
