cd91a65f95934c6a355d6f6658ee7730a929eb45
Otto <heinz-otto.klas@gmx.de>
2023-01-21 15:19:55 +0100

    Update runMqtt.sh

--- monitor/runMqtt.sh
+++ monitor/runMqtt.sh
@@ -4,10 +4,10 @@ TOPIC="monitor/system"
 if ! cmd=$(which mosquitto_pub) ; then echo 'ERROR install mosquitto-clients'; fi
 DIRECTORY=$(cd `dirname $0` && pwd)
 if ! ls ${DIRECTORY}/*.var; then echo wget ... ; fi
-for file in ls ${DIRECTORY}/*.var ; do
+for file in $(ls ${DIRECTORY}/*.var) ; do
   source ${file}
   for varname in $(cat ${file}|awk -F'=' '{print $1}') ;do
-    echo "$cmd -i $(hostname) -h ${MQTT_SVR} -t ${topic}/${varname} -m ${!varname} -q 1"
+    echo "$cmd -i $(hostname) -h ${MQTT_SVR} -t ${TOPIC}/${varname} -m ${!varname} -q 1"
   done
 done
 
