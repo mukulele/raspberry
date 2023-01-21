8bd13f20894fd3510a069d53091f0750b9b0b1a4
Otto <heinz-otto.klas@gmx.de>
2023-01-21 16:48:33 +0100

    Update runMqtt.sh

--- monitor/runMqtt.sh
+++ monitor/runMqtt.sh
@@ -3,7 +3,7 @@ MQTT_SVR="192.168.56.187"
 TOPIC="monitor/system"
 if ! cmd=$(which mosquitto_pub) ; then echo 'ERROR install mosquitto-clients'; fi
 DIRECTORY=$(cd `dirname $0` && pwd)
-if ! ls ${DIRECTORY}/*.var; then echo wget ... ; fi
+if ! ls ${DIRECTORY}/*.var; then echo wget -qO https://raw.githubusercontent.com/heinz-otto/raspberry/master/monitor/{*}.var ; fi
 for file in $(ls ${DIRECTORY}/*.var) ; do
   source ${file}
   for varname in $(cat ${file}|grep -vE '^#'|awk -F'=' '{print $1}') ;do
