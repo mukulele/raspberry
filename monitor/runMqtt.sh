4237ca901de6f81f849b731e50016c833ee90a27
Otto <heinz-otto.klas@gmx.de>
2023-01-21 17:46:54 +0100

    Update runMqtt.sh

--- monitor/runMqtt.sh
+++ monitor/runMqtt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 MQTT_SVR="192.168.56.187"
 TOPIC="monitor/system"
+#ACCOUNT="-u user -P password"
+
 DIRECTORY=$(cd `dirname $0` && pwd)
 if conf=$(ls ${DIRECTORY}/$(basename $0 .sh).conf 2>/dev/null) ; then 
   echo "config $conf vorhanden"
@@ -19,6 +21,6 @@ fi
 for file in $(ls ${DIRECTORY}/*.var) ; do
   source ${file}
   for varname in $(cat ${file}|grep -vE '^#'|awk -F'=' '{print $1}') ;do
-    $cmd -i $(hostname) -h ${MQTT_SVR} -t ${TOPIC}/${varname} -m "${!varname}" -q 1
+    $cmd -i $(hostname) -h ${MQTT_SVR} -t ${TOPIC}/${varname} -m "${!varname}" ${ACCOUNT}
   done
 done
