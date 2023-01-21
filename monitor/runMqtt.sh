b88e55aff94065d016ee1f1212e7e2e4aabea1ca
Otto <heinz-otto.klas@gmx.de>
2023-01-21 17:37:00 +0100

    Update runMqtt.sh

--- monitor/runMqtt.sh
+++ monitor/runMqtt.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 MQTT_SVR="192.168.56.187"
 TOPIC="monitor/system"
-if conf=$(ls $(basename $0 .sh).conf 2>/dev/null) ; then echo "config $conf vorhanden" ; else echo "nehme inside"; fi
+DIRECTORY=$(cd `dirname $0` && pwd)
+if conf=$(ls ${DIRECTORY}/$(basename $0 .sh).conf 2>/dev/null) ; then 
+  echo "config $conf vorhanden"
+  source ${conf}
+else 
+  echo "nehme inside"
+fi
 if ! cmd=$(which mosquitto_pub) ; then 
   echo 'ERROR install mosquitto-clients first'
   exit 1
 fi
-DIRECTORY=$(cd `dirname $0` && pwd)
+
 if ! ls ${DIRECTORY}/*.var; then 
   wget -qN https://raw.githubusercontent.com/heinz-otto/raspberry/master/monitor/{day,hour,second}.var
 fi
