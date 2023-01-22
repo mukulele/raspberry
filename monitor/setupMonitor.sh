86d7efbe80ff29ae9f274087cc37d8cc1accdc55
Otto <heinz-otto.klas@gmx.de>
2023-01-22 09:17:56 +0100

    Create setupMonitor.sh

---
+++ monitor/setupMonitor.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# run this Script as root https://www.linuxjournal.com/content/automatically-re-start-script-root-0
+if [[ $UID -ne 0 ]]; then
+   sudo -p 'Restarting as root, password: ' bash $0 "$@"
+   exit $?
+fi
+### Start Script
+apt update && apt install mosquitto-clients
+export SCRIPT_DIR=$(pwd)
+export SCRIPT_NAME=runMqtt.sh
+wget -qO ${SCRIPT_DIR}/${SCRIPT_NAME} https://raw.githubusercontent.com/heinz-otto/raspberry/master/monitor/${SCRIPT_NAME}
+chmod +x ${SCRIPT_DIR}/${SCRIPT_NAME}
+
+##### create service and timer units
+export NAME=monitor-runMqtt
+cat >/etc/systemd/system/${NAME}@.service <<EOF
+[Unit]
+Description=${NAME}
+[Service]
+Type=oneshot
+ExecStart=${SCRIPT_DIR}/runMqtt.sh %i
+EOF
+
+for i in "hour=*-*-* *:00:00" "day=*-*-* 00:00:00" "second=*:*:0/10"; do  
+FREQ=${i%=*}       # split bevor '='
+VAL=${i#*=}        # split after '='
+# write unit
+cat >/etc/systemd/system/${NAME}-${FREQ}@.timer <<EOF
+[Unit]
+Description=${NAME}-${FREQ} timer
+[Timer]
+Unit=${NAME}@%i.service
+OnCalendar=${VAL}
+Persistent=true
+[Install]
+WantedBy=timers.target
+EOF
+# enable Timer
+systemctl enable --now ${NAME}-${FREQ}@${FREQ}.timer
+done
