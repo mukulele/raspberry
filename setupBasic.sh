41eb9b36695a7c972ab582fef8229f594635c032
mukulele <98270591+mukulele@users.noreply.github.com>
2024-12-18 01:00:12 +0100

    Update setupBasic.sh

--- setupBasic.sh
+++ setupBasic.sh
@@ -5,49 +5,26 @@ if [[ $UID -ne 0 ]]; then
    exit $?
 fi
 ### Start Script
-# Set the new hostname below for quiet setup or leave it blank to be asked
-hname=
-PACKAGE_LANG=(en_US.UTF-8 de_DE.UTF-8) # the first is fallback in LANGUAGE, the last is set to default
-TZ='Europe/Berlin'
 # first load the latest software
 apt -y update
 apt -y full-upgrade
 
-# Timezone
-timedatectl set-timezone $TZ
+# Midnight Commander
+apt-get -y install mc
 
-# config the default language, raspi-config is this doing in a similar way
-for lang in ${PACKAGE_LANG[*]}; do sed -i -e "s/# $lang/$lang/" /etc/locale.gen; done 
-dpkg-reconfigure -f noninteractive locales     # ? locale-gen
-#####
-# localectl set-locale LANG=${PACKAGE_LANG[-1]} LANGUAGE=${PACKAGE_LANG[-1]:0:2}:${PACKAGE_LANG[0]:0:2}
-# update-locale         
-#####
-update-locale LANG=${PACKAGE_LANG[-1]} LANGUAGE=${PACKAGE_LANG[-1]:0:2}:${PACKAGE_LANG[0]:0:2}
+# log2ram
+wget https://github.com/azlux/log2ram/archive/master.tar.gz -O log2ram.tar.gz
+tar xf log2ram.tar.gz
+cd /$PWD/log2ram-master
+./install.sh
 
-# keyboard settings
-localectl set-keymap ${PACKAGE_LANG[-1]:0:2}
-# Code from raspi-config 
-setupcon -k --force <> /dev/tty1 >&0 2>&1
+# journalctl nach 30 Tagen löschen
+journalctl --rotate --vacuum-time=30d
 
-# Hostname
-oname=$(hostname)
-if [ -z $hname ] ; then
-		read -p "Hostname "$oname" ändern? Bitte den neuen Hostnamen eingeben, oder einfach enter:" hnameneu
-		if [[ $hnameneu != "" ]] ; then
-				hname=$hnameneu
-		fi
-fi
-if [[ $hname != "" ]]
-	then
-	hostnamectl set-hostname $hname
-        sed -i s/$oname/$hname/ /etc/hosts
-	sed -i s/$oname/$hname/ /etc/ssh/*.pub
-fi
+# RPI Monitor
+apt-get install sysstat
 
-# Password 
-# echo "linuxpassword" | passwd --stdin linuxuser
+apt autoremove
 
-# Reboot
-# reboot
-echo "Ein reboot ist empfohlen!"
+#Reboot
+echo "Reboot to apply changes\nrun ssh pi@${host}.local"
