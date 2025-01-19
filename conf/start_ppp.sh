8030377ccd95005e284b421444a9451f3e152f0f
mukulele <98270591+mukulele@users.noreply.github.com>
2025-01-19 15:08:32 +0100

    Create start_ppp.sh

---
+++ conf/start_ppp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+#​ ​ /etc/init.d/start_ppp 
+# /etc/ppp/peers/provider remove updetach
+#​ ​chmod​ ​755​ ​ /etc/init.d/start_ppp
+pon​
+ #​ ​ls​​ /etc/rc5.d 
+ #​ ​update-rc.d​ ​start_ppp​ ​defaults​ ​05
