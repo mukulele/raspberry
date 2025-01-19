ac613660091b5a887536e57529325261213fb9ef
mukulele <98270591+mukulele@users.noreply.github.com>
2025-01-19 14:11:26 +0100

    Update gpsdloggerStart.sh

--- conf/gpsdloggerStart.sh
+++ conf/gpsdloggerStart.sh
@@ -1,1 +1,1 @@
-gpxlogger --interval 5 --minmove 20 --output "/home/pi/tracks/$(date '+%F %H:%M:%S').gpx" --reconnect
+gpxlogger --interval 5 --minmove 20 --output "/gpsdtracks/$(date '+%F %H:%M:%S').gpx" --reconnect
