7d75a0db7b362b234e8c340a52c6983b712f2435
mukulele <98270591+mukulele@users.noreply.github.com>
2025-03-01 19:18:20 +0100

    Update ppp-keep-alive.sh

--- conf/ppp-keep-alive.sh
+++ conf/ppp-keep-alive.sh
@@ -1,1 +1,10 @@
-
+#!/bin/sh
+if ping -I ppp0 -c 2 google.com &> /dev/null
+then
+  echo "*** Connection OK ***" >> /var/log/ppp-keep-alive
+else
+  poff
+  wait 5
+  pon
+  echo "*** Connection restarted ***" >> /var/log/ppp-keep-alive
+fi
