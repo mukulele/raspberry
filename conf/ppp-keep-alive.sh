2a3aa55560a7d2deb262abd7045ac8404e01872a
mukulele <98270591+mukulele@users.noreply.github.com>
2025-03-06 21:15:54 +0100

    Update ppp-keep-alive.sh

--- conf/ppp-keep-alive.sh
+++ conf/ppp-keep-alive.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+if [[ $UID -ne 0 ]]; then
+   sudo -p 'Restarting as root, password: ' bash $0 "$@"
+   exit $?
+fi
+
 if ping -I ppp0 -c 2 google.com &> /dev/null
 then
-  echo "*** Connection OK ***" >> /var/log/ppp-keep-alive
+  printf '%(%Y-%m-%d %H:%M:%S)T OK\n' -1 >> /var/log/ppp-keep-alive
+  OUT="$(ifconfig eth0 | grep bytes)"
+  echo "${OUT}" >> /var/log/ppp-keep-alive >> /var/log/keeep-alive
 else
-  poff
-  wait 5
+  printf '%(%Y-%m-%d %H:%M:%S)T FAIL\n' -1 >> /var/log/ppp-keep-alive
   pon
-  echo "*** Connection restarted ***" >> /var/log/ppp-keep-alive
 fi
