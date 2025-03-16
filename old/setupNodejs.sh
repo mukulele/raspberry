710bb70afc16a5438f723d28b2c46b3b12d1766b
mukulele <98270591+mukulele@users.noreply.github.com>
2025-03-16 16:21:42 +0100

    Delete old directory

--- old/setupNodejs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# look for Releases https://nodejs.org/de/about/releases/
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-apt-get install -y nodejs
-apt-get install -y libjson-pp-perl
-npm install pm2 -g
