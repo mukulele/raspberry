f5fbf9f3caa7cb4a93e1adf0ec91d04c8c4b4517
mukulele <98270591+mukulele@users.noreply.github.com>
2025-03-16 16:10:19 +0100

    Update gpsdloggerStart.sh

--- conf/gpsdloggerStart.sh
+++ conf/gpsdloggerStart.sh
@@ -1,1 +1,7 @@
-gpxlogger --interval 5 --minmove 20 --output "/gpsdtracks/$(date '+%F %H:%M:%S').gpx" --reconnect
+#!/bin/bash
+if [[ $UID -ne 0 ]]; then
+   sudo -p 'Restarting as root, password: ' bash $0 "$@"
+   exit $?
+fi
+gpxlogger --interval 5 --minmove 20 --output "/var/log/gpstracks-$(date '+%F %H:%M:%S').gpx" --reconnect
+#gpxlogger --interval 5 --minmove 20 --output "/gpsdtracks/$(date '+%F %H:%M:%S').gpx" --reconnect
