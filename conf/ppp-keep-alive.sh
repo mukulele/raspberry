6936964f30ded03e21ccaa26e83c8277144c77ea
mukulele <98270591+mukulele@users.noreply.github.com>
2025-05-29 20:30:49 +0200

    Update ppp-keep-alive.sh

--- conf/ppp-keep-alive.sh
+++ conf/ppp-keep-alive.sh
@@ -8,7 +8,7 @@ if ping -I ppp0 -c 2 google.com &> /dev/null
 then
   printf '%(%Y-%m-%d %H:%M:%S)T OK\n' -1 >> /var/log/ppp-keep-alive
   OUT="$(ifconfig eth0 | grep bytes)"
-  echo "${OUT}" >> /var/log/ppp-keep-alive >> /var/log/keeep-alive
+  echo "${OUT}" >> /var/log/ppp-keep-alive >> /var/log/keep-alive
 else
   printf '%(%Y-%m-%d %H:%M:%S)T FAIL\n' -1 >> /var/log/ppp-keep-alive
   pon
