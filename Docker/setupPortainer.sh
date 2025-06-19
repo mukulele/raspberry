77bbe3d44af5bf439588a1572acdef456054f168
mukulele <98270591+mukulele@users.noreply.github.com>
2025-06-19 15:34:18 +0200

    Update setupPortainer.sh

--- Docker/setupPortainer.sh
+++ Docker/setupPortainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # portainer as solo container - sudo -su $(id -un) give the trick to get new groupmembership without extra logout/login
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 echo -e "Portainer is running and reachable under\n   http://$(hostname):9000"
